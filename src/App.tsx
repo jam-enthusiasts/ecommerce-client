5bbd824b7d4587b37e32820344e3aab695403361
Corey Lanier <iam@coreylanier.com>
2020-01-28 17:40:15 -0500

    created base header with destructured context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+// Library imports
 import React from "react";
+
+// Context imports
 import UserContextProvider from "./contexts/UserContext";
+
+// Component imports
+import Header from "./components/Header";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <UserContextProvider></UserContextProvider>
+      <UserContextProvider>
+        <Header />
+      </UserContextProvider>
     </div>
   );
 };
