51d88bfac13e6c6d3b52c2c66df221e30c15ab99
Corey Lanier <iam@coreylanier.com>
2020-01-31 19:59:30 -0500

    initial gql load

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,17 +1,35 @@
 import React, { createContext, useState } from "react";
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 
 interface IUser {
   user: String;
   role: String;
+  data?: any;
 }
 
-export const UserContext = createContext<IUser>({ user: "", role: "" });
+const GET_USERS_QUERY = gql`
+  query getUsers {
+    users {
+      email
+      name
+    }
+  }
+`
+type User = {
+  email: string,
+  name: string
+}
+
+export const UserContext = createContext<IUser>({ user: "", role: "", data: [] });
 
 const UserContextProvider: React.FC = props => {
+  const { loading, error, data } = useQuery(GET_USERS_QUERY);
+  console.log(data);
   const [user, setUser] = useState("");
   const [role, setRole] = useState("");
   return (
-    <UserContext.Provider value={{ user, role }}>
+    <UserContext.Provider value={{ user, role, data }}>
       {props.children}
     </UserContext.Provider>
   );
