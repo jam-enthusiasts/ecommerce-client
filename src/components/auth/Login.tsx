4bd2dd726ff035143e24a760e24dacf059da5730
unknown <onew1ng3d@gmail.com>
2020-02-02 02:22:36 -0500

    basic register and login component fleshed out; TDL: add login token to localstorage

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,13 +1,41 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
+import { useForm } from "react-hook-form";
 
 import { useMutation } from '@apollo/react-hooks';
+import { SIGNIN_MUTATION } from "../../gql/mutations/auth";
+
+type FormData = {
+	name: string;
+	email: string;
+	password: string;
+};
 
 //Login form component
 const Login: React.FC = () => {
+	const [user, setUserLogin] = useState({});
+
+	const { register, handleSubmit, errors } = useForm<FormData>();
+	const onSubmit = handleSubmit((data) => {
+		setUserLogin(data);
+		signIn();
+	});
+
+	const [signIn, { loading, data, error }] = useMutation(
+		SIGNIN_MUTATION,
+		{
+			variables: user
+		}
+	)
+
 	return (
-		<form action="">
-			<input type="text" name="test" id="test" />
-			<input type="submit" value="Login" />
+		<form onSubmit={onSubmit}>
+			<label>Name</label>
+			<input name="name" ref={register} />
+			<label>Email</label>
+			<input name="email" ref={register} />
+			<label>Password</label>
+			<input name="password" ref={register} />
+			<input type="submit" value="Submit" />
 		</form>
 	)
 }
--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -1,28 +1,45 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { useForm } from "react-hook-form";
 
 import { useMutation } from '@apollo/react-hooks';
+import { SIGNUP_MUTATION } from "../../gql/mutations/auth";
 
 
 type FormData = {
-	firstName: string;
-	lastName: string;
+	name: string;
+	email: string;
+	password: string;
+	password2: string;
 };
 
 //Register form component
-const Register = () => {
-	const { register, setValue, handleSubmit, errors } = useForm<FormData>();
-	const onSubmit = handleSubmit(({ firstName, lastName }) => {
-		console.log(firstName, lastName);
-	}); // firstName and lastName will have correct type
+const Register: React.FC = () => {
+	const [newUser, setNewUserData] = useState({});
+
+	const { register, handleSubmit, errors } = useForm<FormData>();
+	const onSubmit = handleSubmit((data) => {
+		setNewUserData(data);
+		signUp();
+	});
+
+	const [signUp, { loading, data, error }] = useMutation(
+		SIGNUP_MUTATION,
+		{
+			variables: newUser
+		}
+	)
+
 
 	return (
 		<form onSubmit={onSubmit}>
-			<label>First Name</label>
-			<input name="firstName" ref={register} />
-			<label>Last Name</label>
-			<input name="lastName" ref={register} />
-
+			<label>Name</label>
+			<input name="name" ref={register} />
+			<label>Email</label>
+			<input name="email" ref={register} />
+			<label>Password</label>
+			<input name="password" ref={register} />
+			<label>Confirm Password</label>
+			<input name="password2" ref={register} />
 			<input type="submit" value="Submit" />
 		</form>
 	);
